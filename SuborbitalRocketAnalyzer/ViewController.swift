a0d950f5360ee1806feca1a339279b878debea3e
John Ahrens <john@john-ahrens.com>
2014-11-30 17:12:46 -0700

    Building UI
    UI built except for the IBAction calculating results.

--- SuborbitalRocketAnalyzer/ViewController.swift
+++ SuborbitalRocketAnalyzer/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var payloadInput: UITextField!
+    @IBOutlet weak var targetAltitudeInput: UITextField!
+    @IBOutlet weak var averageIspInput: UITextField!
+    @IBOutlet weak var thrustDurationInput: UITextField!
+    @IBOutlet weak var gravityLossesInput: UITextField!
+    @IBOutlet weak var aeroLossesInput: UITextField!
+    @IBOutlet weak var propellantRatioLabel: UILabel!
+    @IBOutlet weak var payloadMass: UILabel!
+    @IBOutlet weak var structuralMass: UILabel!
+    @IBOutlet weak var propellantMass: UILabel!
+    @IBOutlet weak var liftoffMass: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        propellantRatioLabel.text = "80%"
+        payloadMass.text = "Payload Mass:"
+        structuralMass.text = "Structural Mass:"
+        propellantMass.text = "Propellant Mass:"
+        liftoffMass.text = "Gross Liftoff Mass:"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func propellantRatioChanged(sender: UISlider) {
+        let pr = lroundf(sender.value)
+        propellantRatioLabel.text = "\(pr)%"
+    }
 
+    @IBAction func backgroundTab(sender: UIControl) {
+        payloadInput.resignFirstResponder()
+        targetAltitudeInput.resignFirstResponder()
+        averageIspInput.resignFirstResponder()
+        thrustDurationInput.resignFirstResponder()
+        gravityLossesInput.resignFirstResponder()
+        aeroLossesInput.resignFirstResponder()
+    }
+
+    @IBAction func calculatePerformance(sender: UIButton) {
+    }
 }
 
